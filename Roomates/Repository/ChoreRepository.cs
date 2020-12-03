65d9aea83c9917cf14540b1fcac1e47cbe40a16f
Joe <overtonje@gmail.com>
2020-12-03 15:14:10 -0600

    finished chore repo methods

---
+++ Roomates/Repository/ChoreRepository.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Data.SqlClient;
+using Roomates.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roomates.Repository
+{
+    class ChoreRepository : BaseRepository
+    {
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+        public List<Chore> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Chore> chores = new List<Chore>();
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+                        int IdValue = reader.GetInt32(idColumnPosition);
+
+                        int nameColumnPosition = reader.GetOrdinal("Name");
+                        string nameValue = reader.GetString(nameColumnPosition);
+
+                        Chore chore = new Chore
+                        {
+                            Id = IdValue,
+                            Name = nameValue,
+                        };
+                        chores.Add(chore);
+                    }
+                    reader.Close();
+                    return chores;
+                }
+            }    
+        }
+
+        public Chore GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                { 
+                cmd.CommandText = "SELECT Name FROM Chore WHERE Id = @id"
+                        cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Chore chore = null;
+
+                        if (reader.Read())
+                    {
+                        chore = new Chore
+                        {
+                            Id = id,
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                        }; 
+                            }
+                    reader.Close();
+                    return chore;
+                }
+            }
+        }
+
+
+        public void Insert(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                { 
+                cmd.CommandText = @"INSERT INTO Chore (Name) 
+                                             OUTPUT INSERTED.Id 
+                                             VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    int id = (int)cmd.ExecuteScalar();
+
+                    chore.Id = id;
+
+                }
+            }
+        }
+    }
+}
